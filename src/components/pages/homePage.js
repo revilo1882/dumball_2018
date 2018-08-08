5d557b8e7fdcb2e97f3589f9da0aa490c03970af
Revilo1882 <oliverrharris@gmail.com>
2018-08-08 11:53:23 +0100

    Add content to our story

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -6,11 +6,57 @@ class Homepage extends Component {
         <div className="container-fluid">
 
         <h1>
-          Home Page
+          Our Story
         </h1>
 
         <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        The Indian Drifters are back!!....and this time it{"'"}s personal (it’s
+          literally no more personal than last time).
+        </p>
+        <p>
+        We (Matt Reade, Ashley Johnson and Matt Joyce) had so much fun taking
+        part and raising money for Dumball 2016 we wanted to do it all again in
+        2018. So here we are, the band are back together!!
+        </p>
+        <p>
+        This time round we{"'"}ll be driving from Margate to Corfu cooped up in
+        a clapped out old banger recording our progress (lack of it) for your
+        amusement. You can expect more blogging, Instagramming and much, much
+        more ridiculous fancy dress as we try to navigate our way across 2000km
+        of Europeans lesser known roads.
+        </p>
+        <p>
+        We will be raising money for the Teenage Cancer Trust. Most of us
+        probably know someone who has been affected by cancer. I wouldn’t even
+        want to imagine how devastating this must be for someone in their
+        teenage years.
+        </p>
+        <p>
+        Every penny we raise goes directly to the charity to help provide
+        life-changing care and support so young people don’t have to face cancer
+        alone. Please help us raise as much money as possible!
+        through JustGiving is simple, fast and totally secure. Your details are
+        safe with JustGiving –they’ll never sell them on or send unwanted
+        emails. Once you donate, they’ll send your money directly to the
+        charity. So it’s the most efficient way to donate – saving time and
+        cutting costs for the charity.
+        </p>
+        <p>
+        It is also important to mention that the trip, the car, the awesome
+        fancy dress is all funded by us. 100% of your donation go’s straight
+        to the Teenage Cancer Trust.
+        </p>
+        <p>
+        They’ll be tears, tantrums and tyre trouble…. but once we get out of
+        Margate we’ll be fine 😉.  We{"'"}d be honoured if you follow our
+        journey and spare a few ££ to help us raise as much money for Teenage
+        Cancer Trust as possible. The charity are truly remarkable.
+        </p>
+        <p>
+        Much Love
+        </p>
+        <p>
+        Ash, Matt & Matt xx
         </p>
 
         </div>
