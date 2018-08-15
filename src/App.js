79bcd700e9a7b07a6725503e6a137524da5f3b4e
Revilo1882 <oliverrharris@gmail.com>
2018-08-15 11:06:02 +0100

    Add mobile view settings

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import {
 } from 'react-router-dom'
 
 import Header from './components/headerComponent/header';
+import MobileHeader from './components/headerComponent/mobileHeader';
 import Homepage from './components/pages/homePage';
+import MobileHomepage from './components/pages/mobileHomePage';
 import Journey from './components/pages/journey';
 import Photos from './components/pages/photos';
 
@@ -13,7 +15,47 @@ import Photos from './components/pages/photos';
 import './Assets/css/default.min.css'
 
 class App extends Component {
-  render() {
+  constructor() {
+  super();
+  this.state = {
+    width: window.innerWidth,
+  };
+}
+
+componentWillMount() {
+  window.addEventListener('resize', this.handleWindowSizeChange);
+}
+
+// make sure to remove the listener
+// when the component is not mounted anymore
+componentWillUnmount() {
+  window.removeEventListener('resize', this.handleWindowSizeChange);
+}
+
+handleWindowSizeChange = () => {
+  this.setState({ width: window.innerWidth });
+};
+
+render() {
+  const { width } = this.state;
+  const isMobile = width <= 500;
+  // the rest is the same...
+
+  if (isMobile) {
+    return (
+      <Router>
+      <div className="App">
+
+      <MobileHeader />
+
+      <Route exact path="/" component={MobileHomepage} />
+      <Route exact path="/Journey" component={Journey} />
+      <Route exact path="/Photos" component={Photos} />
+
+      </div>
+      </Router>
+    );
+  } else {
     return (
       <Router>
       <div className="App">
@@ -28,6 +70,7 @@ class App extends Component {
       </Router>
     );
   }
+}
 }
 
 export default App;
---
+++ src/components/headerComponent/mobileHeader.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+  Link
+} from 'react-router-dom'
+
+class MobileHeader extends Component {
+  render() {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <li className="first">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Journey">Journey</Link>
+            </li>
+            <li>
+              <Link to="/Photos">2016</Link>
+            </li>
+            <li className="last">
+              <a href = 'https://www.justgiving.com/fundraising/matt-reade1'>Donate</a>
+            </li>
+          </ul>
+        </nav>
+
+      </header>
+    );
+  }
+}
+
+export default MobileHeader;
